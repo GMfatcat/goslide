250ca732cd6e8b2e9c055da7afa847fbf247b28a
GMfatcat <a60102244@gmail.com>
2026-04-16 21:01:33 +0800

    feat(ir): add intermediate representation types

---
+++ internal/ir/presentation.go
@@ -0,0 +1,55 @@
+package ir
+
+import "html/template"
+
+type Presentation struct {
+	Source   string
+	Meta     Frontmatter
+	Slides   []Slide
+	Warnings []Error
+}
+
+type Slide struct {
+	Index    int
+	Meta     SlideMeta
+	RawBody  string
+	BodyHTML template.HTML
+	Regions  []Region
+}
+
+type Region struct {
+	Name string
+	HTML template.HTML
+}
+
+type SlideMeta struct {
+	Title         string
+	Layout        string
+	Transition    string
+	Fragments     bool
+	FragmentStyle string
+}
+
+type Frontmatter struct {
+	Title         string   `yaml:"title"`
+	Author        string   `yaml:"author"`
+	Date          string   `yaml:"date"`
+	Tags          []string `yaml:"tags"`
+	Theme         string   `yaml:"theme"`
+	Accent        string   `yaml:"accent"`
+	Transition    string   `yaml:"transition"`
+	Fragments     bool     `yaml:"fragments"`
+	FragmentStyle string   `yaml:"fragment-style"`
+}
+
+type Error struct {
+	Slide    int
+	Severity string
+	Code     string
+	Message  string
+	Hint     string
+}
+
+func (e Error) IsError() bool {
+	return e.Severity == "error"
+}
